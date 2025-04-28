b6fc26294199868026a45c073e9731a4119c1149
muffinweb <ugurcengiztr35@gmail.com>
2025-04-28 11:53:32 +0300

    app.py fixed

--- scrapers/api/app.py
+++ scrapers/api/app.py
@@ -31,7 +31,7 @@ def read_root():
 @app.get("/scrape/hyundai/{blNumber}")
 async def hyundai_scrape(blNumber: str):
 
-    SCRAPE_COMMAND = "python3 /SeleniumBase/scrapers/api/carriers/hyundai/turkon-scraper.py " + str(blNumber)
+    SCRAPE_COMMAND = "python3 /SeleniumBase/scrapers/api/carriers/hyundai/hyundai-scrape.py " + str(blNumber)
     
     # General Benchmark START
     start_time = time.time()  # Başlangıç zamanı
@@ -57,31 +57,3 @@ async def hyundai_scrape(blNumber: str):
     general_benchmark_time = f"Geçen süre: {elapsed_time:.5f} saniye"
 
     return {"return_code": return_code, "general_benchmark": general_benchmark_time, "result": parsed_json_output}
-
-
-@app.get("/hyundai/daemon/{trackingNumber}")
-async def hyundai_daemon_scrape(trackingNumber: str):
-
-    # If server is windows
-    WIN_COMMAND = "docker run -it --rm -v C:/Users/Shipsgo_Ugur/Desktop/seleniumbasedocker/SeleniumBase/scrapers:/SeleniumBase/scrapers:rw seleniumbase python3 carriers/hyundai/turkon-scraper.py " + str(trackingNumber)
-
-    proxy_token_for_linux = os.getenv("PROXY_TOKEN")
-
-    # server is linux
-    LIN_COMMAND = "docker run -it --rm -e PROXY_TOKEN -v /root/srv/seleniumbase/SeleniumBase/scrapers:/SeleniumBase/scrapers:rw seleniumbase python3 /SeleniumBase/scrapers/api/carriers/hyundai/turkon-scraper.py " + str(trackingNumber)
-
-    result = subprocess.run(
-        args=LIN_COMMAND,
-        shell=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        check=True,
-        text=True,
-        encoding="utf-8"
-    )
-
-    output = result.stdout
-    return_code = result.returncode
-
-    return {"return_code": return_code, "result": output}
-
